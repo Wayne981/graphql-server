c5bf24439c4e6f6c53a19934dd7aa5c26deca3dc
Chiranthan M <chiranthanmahesh007@gmail.com>
2024-08-27 15:33:30 +0530

    prisma setup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
+import { prismaClient } from "./lib/db";
 
 async function init() {
     const app = express();
@@ -13,11 +14,33 @@ async function init() {
             hello: String
             say(name: String!): String
           }
+          type Mutation {
+            createUser(firstName: String!, lastName: String!, email: String!, password: String!): Boolean
+          }
         `, // schema
         resolvers: {
           Query: {
             hello: () => `Hey there, I am a GraphQL server illaya`,
             say: (_, {name}: {name: string}) => `Hey ${name}, How are u ilka?`
+          },
+          Mutation: {
+            createUser: async(_, {firstName, lastName, email, password}: {
+              firstName: string;
+              lastName: string;
+              email: string;
+              password: string;
+            }) => {
+              await prismaClient.user.create({
+                data: {
+                  email, 
+                  firstName, 
+                  lastName,
+                  password, 
+                  salt: "random_salt",
+                },
+              });
+              return true;
+            }
           }
         },
     }); // schema layer
